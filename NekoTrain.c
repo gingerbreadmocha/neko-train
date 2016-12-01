04830e8aeff8d938c69008b89285f852b2f430bd
gingerbreadmocha <jennyzhou0302@gmail.com>
2016-11-30 17:27:50 -0800

    Add Printfunc and Neko struct

--- NekoTrain.c
+++ NekoTrain.c
@@ -1,6 +1,28 @@
 #include "stdio.h"
+#include "string.h"
+#include "stdlib.h"
+
+/* Usually use pointers for struct. */
+typedef struct Neko{
+	char *name;
+	char *personality;
+} Neko;
+
+void printNeko(Neko *neko){
+	printf("Neko name: %s \n Neko personality: %s \n",neko->name,neko->personality);
+}
+
 
 int main(){
 	printf("Welcome to Neko Train! \n");
+	Neko *Pumpkin; /*For typedef, you don't need to put struct in front of ur 'struct' anymore.*/
+	
+	/*Pumpkin's information */
+	Pumpkin=malloc(sizeof(Neko));
+	Pumpkin->name="Pumpkin";
+	Pumpkin->personality="Spacey";
+
+	printNeko(Pumpkin);
+
 	return 0;
 }
